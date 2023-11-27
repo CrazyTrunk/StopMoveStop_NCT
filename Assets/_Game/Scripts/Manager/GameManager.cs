e7f1f11a1955bc5247f87dd3bbd745db9057607e
CrazyTrunk <ncongtrung1610@gmail.com>
2023-11-27 15:53:25 +0700

    Adding Material

---
+++ Assets/_Game/Scripts/Manager/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GameManager : Singleton<GameManager>
+{
+    private GameState _gameState;
+    private void Awake()
+    {
+        ChangeState(GameState.MainMenu);
+    }
+    public void ChangeState(GameState state)
+    {
+        _gameState = state;
+    }
+    public bool IsState(GameState gameState)
+    {
+        return _gameState == gameState;
+    }
+}
