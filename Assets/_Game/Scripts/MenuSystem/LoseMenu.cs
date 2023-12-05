4f63fcb8100addec4ff6fa7f7d19a5f242666504
CrazyTrunk <ncongtrung1610@gmail.com>
2023-12-06 02:53:34 +0700

    Backup Process

--- Assets/_Game/Scripts/MenuSystem/LoseMenu.cs
+++ Assets/_Game/Scripts/MenuSystem/LoseMenu.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoseMenu : Menu<LoseMenu>
 {
     [SerializeField] TextMeshProUGUI rankText;
     [SerializeField] TextMeshProUGUI killedByText;
     [SerializeField] TextMeshProUGUI coinGained;
-
+    private PlayerData playerData;
+    [SerializeField] Image[] ZoneImages;
+    []
+    private void Start()
+    {
+        playerData = GameManager.Instance.GetPlayerData();
+    }
     public void OnInit(int rank, string killer, int coinGain)
     {
         rankText.text = $"#{rank}" ;
