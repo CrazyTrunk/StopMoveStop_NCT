747e5edc69e35beb1eb6d1e33d1bfeed13becb72
CrazyTrunk <ncongtrung1610@gmail.com>
2023-12-14 02:22:56 +0700

    Backup Icon

--- Assets/_Game/Scripts/MenuSystem/LoseMenu.cs
+++ Assets/_Game/Scripts/MenuSystem/LoseMenu.cs
@@ -10,21 +10,33 @@ public class LoseMenu : Menu<LoseMenu>
     [SerializeField] TextMeshProUGUI killedByText;
     [SerializeField] TextMeshProUGUI coinGained;
     private PlayerData playerData;
-    [SerializeField] Image[] ZoneImages;
+    [SerializeField] private Sprite[] ZoneImages;
+    [SerializeField] private Image currentZone;
+    [SerializeField] private Image nextZone;
+    [SerializeField] private TextMeshProUGUI currentZoneText;
+    [SerializeField] private TextMeshProUGUI nextZoneText;
 
     [SerializeField] private Slider currentSlider;
     [SerializeField] private Slider highScoreSlider;
     private void Start()
     {
+        OnInit();
+    }
+    public void OnInit()
+    {
         playerData = GameManager.Instance.GetPlayerData();
         CalculateHighScore();
+        currentZoneText.text = $"Zone {playerData.levelMap}";
+        nextZoneText.text = $"Zone {playerData.levelMap + 1}";
+        currentZone.sprite = ZoneImages[playerData.levelMap - 1];
+        nextZone.sprite = ZoneImages[playerData.levelMap];
     }
     private void CalculateHighScore()
     {
         //sliderValue = (1 - (ranking - 1) / (totalPlayers - 1)) * maxValueSlider
         //Bước này đảo ngược tỷ lệ phần trăm
         int highestRank = playerData.GetHighestScoreByLevel(playerData.levelMap);
-        float reversePercent =(float)(highestRank - 1) / LevelManager.Instance.MaxParticipants;
+        float reversePercent = (float)(highestRank - 1) / LevelManager.Instance.MaxParticipants;
         float reverseToPercent = 1 - reversePercent;
         highScoreSlider.value = reverseToPercent * 11.5f;
     }
