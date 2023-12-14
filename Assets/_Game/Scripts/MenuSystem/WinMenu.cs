0a1b792faf578c983ab863c513f6eafc04d710ac
CrazyTrunk <ncongtrung1610@gmail.com>
2023-12-14 16:40:34 +0700

    Win Menu

--- Assets/_Game/Scripts/MenuSystem/WinMenu.cs
+++ Assets/_Game/Scripts/MenuSystem/WinMenu.cs
@@ -1,15 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WinMenu : Menu<WinMenu>
 {
     private PlayerData playerData;
+    [SerializeField] private Sprite[] ZoneImages;
+    [SerializeField] private Image currentZone;
+    [SerializeField] private Image nextZone;
+    [SerializeField] private TextMeshProUGUI currentZoneText;
+    [SerializeField] private TextMeshProUGUI nextZoneText;
     private void Start()
     {
+        OnInit();
+        currentZoneText.text = $"Zone {playerData.levelMap - 1}";
+        nextZoneText.text = $"Zone {playerData.levelMap}";
+        currentZone.sprite = ZoneImages[playerData.levelMap - 2];
+        nextZone.sprite = ZoneImages[playerData.levelMap - 1];
+    }
+    public void OnInit()
+    {
         playerData = GameManager.Instance.GetPlayerData();
+
     }
     public void OnNextZoneClicked()
     {
